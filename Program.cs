ddb90b16eaad3f7dace0984cfa0a79812ffc52ba
student <student@DESKTOP-EFORU69>
2022-09-29 09:33:10 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            string y = "";
+
+            while (a != 0) 
+            {
+                int x = a % 2;
+
+                y += x;
+
+                a = (a / 2);
+            }
+
+            Console.WriteLine(string.Join("",y.ToCharArray().Reverse()));
+            Console.ReadKey();
+        }
+    }
+}
